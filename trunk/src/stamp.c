1d53f05114895d494be835a93c56a788d78e11f8
j.howard.johnson <j.howard.johnson@95846149-86d8-620c-2f8d-3a5ce498ed98>
2010-07-26 19:55:32 +0000

    Add text needed for GPLv3 to clarify users rights.

--- trunk/src/stamp.c
+++ trunk/src/stamp.c
@@ -1,11 +1,31 @@
 /*
  * Copyright (c) 1987, J Howard Johnson, University of Waterloo.
- * Not for sale or distribution without written permission of the author.
+ *
+ * This software was developed while I was a student and, later, professor
+ * at the University of Waterloo.  It has only minimal enhancements and bug
+ * fixes from later than August 1988.  It was released under the GPLv3
+ * licence on July 26, 2010.
+ *                 -- J Howard Johnson ( j.howard.johnson *at* gmail.com )
+ *
+ * This file is part of INR.
+ *
+ *   INR is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   INR is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with INR.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 /*
  * Name and modification date of the current version
  */
 
-char Version[] = "2.0e (patched)";
-char Date[] = "Mar 25 1988 (patched Jun 17 2010)";
+char Version[] = "2.0.1";
+char Date[] = "July 26, 2010";
