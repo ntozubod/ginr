554faa99ade1c9c75b9755889d4e36d708550b7b
J Howard Johnson <j.howard.johnson@gmail.com>
2018-09-10 12:32:10 -0400

    Make some changes to Makefile to reformat to 1tbs using astyle. Perform make format to achieve rebormatting.

--- src/util.c
+++ src/util.c
@@ -33,30 +33,30 @@ extern FILE *fpout;
 Warning( s )
 char *s;
 {
-	fprintf( fpout, "*** %s ***\n", s );
+  fprintf( fpout, "*** %s ***\n", s );
 }
 
 Error( s )
 char *s;
 {
-	fprintf( fpout, "*** %s ***\n", s );
-	fflush( fpout );
-	exit( 1 );
+  fprintf( fpout, "*** %s ***\n", s );
+  fflush( fpout );
+  exit( 1 );
 }
 
 SHORT *s_alloc( n )
 int n;
 {
-	return( (SHORT *) Salloc( n * sizeof( SHORT ) ) );
+  return( ( SHORT * ) Salloc( n * sizeof( SHORT ) ) );
 }
 
 int *i_alloc( n )
 int n;
 {
-	return( (int *) Salloc( n * sizeof( int ) ) );
+  return( ( int * ) Salloc( n * sizeof( int ) ) );
 }
 
 pr_time_diff()
 {
-	return;
+  return;
 }
